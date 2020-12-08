bd86de559ca2ae27b2a9e283e0578aca0552d456
Gilang Sinawang <giirugiru@gmail.com>
2020-12-08 13:23:53 +0700

    First Commit

--- Assets Exercise/ViewController.swift
+++ Assets Exercise/ViewController.swift
@@ -6,14 +6,53 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var albumImage: UIImageView!
+    @IBOutlet weak var firstButtonLabel: UIButton!
+    
+    var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Sample image on load
+        albumImage.image = #imageLiteral(resourceName: "Sample")
     }
 
-
+    @IBAction func KPopButton(_ sender: Any) {
+        playMusic(musicName: "KPOP")
+        changeImage(imageName: "KPOP")
+    }
+    
+    @IBAction func JazzButton(_ sender: Any) {
+        playMusic(musicName: "JAZZ")
+        changeImage(imageName: "JAZZ")
+    }
+    
+    @IBAction func ChillButton(_ sender: Any) {
+        playMusic(musicName: "CHILL")
+        changeImage(imageName: "CHILL")
+    }
+    
+    // Play Music
+    func playMusic(musicName: String) {
+        let path = Bundle.main.path(forResource: "\(musicName)", ofType: "mp3")!
+        let url = URL(fileURLWithPath: path)
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+        } catch {
+            print("Error")
+        }
+    }
+    
+    // Change Image with dissolve transition
+    func changeImage(imageName: String) {
+        UIView.transition(with: albumImage, duration: 0.7, options: .transitionCrossDissolve, animations: {
+            self.albumImage.image = UIImage(named: "\(imageName)")
+        }, completion: nil)
+    }
 }
 
---
+++ readme.md
@@ -0,0 +1,9 @@
+
+Simple project for learning purposes
+
+To understand about assets, how to fetch data from assets & display it on the view
+Also a bit about AVFoundation to play Music from the app directory (or we call it bundle)
+Using buttons to play music & change the image
+
+Don't mind the music selection, I randomly picked it from my recently downloaded files.
+The jazz music is not even a jazz music, but whatever.
